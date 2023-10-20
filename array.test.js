24e4724f0a65aaa0247ff2551bd84e4782557eed
Camilo Isaza <camiloisazag@hotmail.com>
2023-10-20 09:38:04 +0200

    Add new function modules

--- array.test.js
+++ array.test.js
@@ -4,7 +4,10 @@ import {
   popArray,
   unshiftArray,
   shiftArray,
-  someArray,
+  includesArray,
+  indexOfArray,
+  reduceOfArray,
+  joinArray,
 } from './array.js';
 
 describe('Given arrayLength', () => {
@@ -33,9 +36,10 @@ describe('Given arrayLength', () => {
 describe('Given pushArray', () => {
   test('should first', () => {
     const testData = [1, 2, 3, 9];
+    const x = 10;
     const expected = 5;
-    const result = pushArray(testData);
-    expect(result).toBe(expected);
+    const result = pushArray(testData, x);
+    expect(result).toStrictEqual(expected);
   });
 
   test('should first', () => {
@@ -150,28 +154,12 @@ describe('Given shiftArray', () => {
   });
 });
 
-describe('Given someArray', () => {
-  test('should first', () => {
-    const testData = [1, 5, 3, 9];
-    const x = (element) => element % 2 === 0;
-    const expected = false;
-    const result = someArray(testData, x);
-    expect(result).toBe(expected);
-  });
-
-  test('should first', () => {
-    const testData = [1, 2, 3, 9];
-    const x = (element) => element % 2 === 0;
-    const expected = false;
-    const result = someArray(testData, x);
-    expect(result).toBe(expected);
-  });
-
+describe('Given includesArray', () => {
   test('should first', () => {
     const testData = [1, 5, 3, 9];
     const x = 9;
     const expected = true;
-    const result = someArray(testData, x);
+    const result = includesArray(testData, x);
     expect(result).toBe(expected);
   });
 
@@ -179,7 +167,150 @@ describe('Given someArray', () => {
     const testData = [1, 5, 3, 9];
     const x = 8;
     const expected = false;
-    const result = someArray(testData, x);
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = null;
+    const expected = false;
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [[], []];
+    const x = [];
+    const expected = false;
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('Given includesArray', () => {
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = 9;
+    const expected = true;
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = 8;
+    const expected = false;
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = null;
+    const expected = false;
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [[], []];
+    const x = [];
+    const expected = false;
+    const result = includesArray(testData, x);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('Given indexOfArray', () => {
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = 9;
+    const expected = 3;
+    const result = indexOfArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = 8;
+    const expected = -1;
+    const result = indexOfArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = null;
+    const expected = -1;
+    const result = indexOfArray(testData, x);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [[], []];
+    const x = [];
+    const expected = -1;
+    const result = indexOfArray(testData, x);
+    expect(result).toStrictEqual(expected);
+  });
+});
+
+describe('Given reduceArray', () => {
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = 0;
+    const op = (a, b) => a + b;
+    const expected = 18;
+    const result = reduceOfArray(testData, x, op);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const x = 0;
+    const op = (a, b) => a + b;
+    const expected = 18;
+    const result = reduceOfArray(testData, x, op);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 10, 100];
+    const x = 0;
+    const op = (a, b) => {
+      if (a > b) {
+        return a;
+      }
+
+      return b;
+    };
+
+    const expected = 100;
+    const result = reduceOfArray(testData, x, op);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('Given joinArray', () => {
+  test('should first', () => {
+    const testData = ['A', 'B', 'C', 'D'];
+    const expected = 'A-B-C-D';
+    const result = joinArray(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [1, 5, 3, 9];
+    const expected = '1-5-3-9';
+    const result = joinArray(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('should first', () => {
+    const testData = [null, null, 3, 'H'];
+    const expected = 'null-null-3-H';
+    const result = joinArray(testData);
     expect(result).toBe(expected);
   });
 });
